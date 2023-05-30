b6a12f4f2048aa5f85d9eaf0ddee913fc67d611c
MohrDJ <gustavomohr2002dj@gmail.com>
2023-05-30 20:29:45 -0300

    correcao formulario login

--- src/app/login.js
+++ src/app/login.js
@@ -1,13 +1,15 @@
+"use client"
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
+
 
 const schema = yup.object().shape({
   login: yup.string().required('Campo obrigatório'),
   password: yup.string().required('Campo obrigatório')
 });
 
-export default function Login() {
+export default function FormularioLogin() {
   const [loginError, setLoginError] = useState(null);
   const { register, handleSubmit, errors } = useForm({
     validationSchema: schema
@@ -29,16 +31,16 @@ export default function Login() {
   return (
     <div>
       <h1>Login</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={() => handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="login">Login:</label>
-          <input type="text" id="login" name="login" ref={register} />
-          {errors.login && <p>{errors.login.message}</p>}
+          <input type="text" id="login" name="login" {...register("login")} />
+          {errors?.login && <p>{errors.login.message}</p>}
         </div>
         <div>
           <label htmlFor="password">Senha:</label>
-          <input type="password" id="password" name="password" ref={register} />
-          {errors.password && <p>{errors.password.message}</p>}
+          <input type="password" id="password" name="password"  {...register("password")} />
+          {errors?.password && <p>{errors.password.message}</p>}
         </div>
         {loginError && <p>{loginError}</p>}
         <button type="submit">Entrar</button>
